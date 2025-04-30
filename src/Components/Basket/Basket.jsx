8876be633426bad5b2e8f16cc4a7f43f845f0fdf
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-04-30 17:40:10 +0400

    Basket page created

---
+++ src/Components/Basket/Basket.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import bask from "./Basket.module.css"
+
+function Basket() {
+  return (
+    <div>Basket</div>
+  )
+}
+
+export default Basket
---
+++ src/Pages/BasketPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Basket from '../Components/Basket/Basket'
+import Navbar from "../Shared/Navbar/Navbar"
+
+function BasketPage() {
+  return (
+    <div>
+     <Navbar/>
+    <Basket/>
+    </div>
+  )
+}
+
+export default BasketPage
